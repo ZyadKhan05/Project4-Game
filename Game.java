7e4dd31f429f717c043235768573dbceb664f974
Zyad <zhk6319@gmail.com>
2024-02-26 19:38:36 -0500

    New Commit

--- Game.java
+++ Game.java
@@ -1,7 +1,5 @@
 package game;
 
-kjkjkjkj
-
 /*
 CLASS: Game
 DESCRIPTION: A painted canvas in its own window, updated every tenth second.
@@ -10,46 +8,55 @@ NOTE: You don't need to understand the details here, no fiddling neccessary.*/
 import java.awt.*;
 import java.awt.event.*;
 
-//Test
-// hii we can just use te online verison of Vscode then
-// yup
-// https://code.visualstudio.com/docs/java/java-tutorial
+// Follow this: https://code.visualstudio.com/docs/java/java-tutorial
+// Install Git: https://git-scm.com/download/win
+// Run the commands off here: https://stackoverflow.com/questions/54876421/make-sure-you-configure-your-user-email-and-user-name-in-git-when-trying-t
+// Commits should work
 abstract class Game extends Canvas {
   protected boolean on = true;
   protected int width, height;
   protected Image buffer;
-  
-	public Game(String name, int inWidth, int inHeight) {
-	  width = inWidth;
-	  height = inHeight;
-	  
-	  // Frame can be read as 'window' here.
+
+  public Game(String name, int inWidth, int inHeight) {
+    width = inWidth;
+    height = inHeight;
+
+    // Frame can be read as 'window' here.
     Frame frame = new Frame(name);
     frame.add(this);
-    frame.setSize(width,height);
+    frame.setSize(width, height);
     frame.setVisible(true);
     frame.setResizable(false);
-    frame.addWindowListener(new WindowAdapter() { 
-      public void windowClosing(WindowEvent e) {System.exit(0);} 
+    frame.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        System.exit(0);
+      }
     });
-    
+
     buffer = createImage(width, height);
-	}
-  
+  }
+
   // 'paint' will be called every tenth of a second that the game is on.
-	abstract public void paint(Graphics brush);
-  
+  abstract public void paint(Graphics brush);
+
   // 'update' paints to a buffer then to the screen, then waits a tenth of
   // a second before repeating itself, assuming the game is on. This is done
   // to avoid a choppy painting experience if repainted in pieces.
   public void update(Graphics brush) {
     paint(buffer.getGraphics());
-		brush.drawImage(buffer,0,0,this);
-    if (on) {sleep(10); repaint();}
+    brush.drawImage(buffer, 0, 0, this);
+    if (on) {
+      sleep(10);
+      repaint();
+    }
   }
-  
+
   // 'sleep' is a simple helper function used in 'update'.
   private void sleep(int time) {
-    try {Thread.sleep(time);} catch(Exception exc){};
+    try {
+      Thread.sleep(time);
+    } catch (Exception exc) {
+    }
+    ;
   }
 }
