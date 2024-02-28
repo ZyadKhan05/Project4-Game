5a28d99e400b90bb401c72afca9bce84b0fbed3a
Zyad <zhk6319@gmail.com>
2024-02-28 15:27:24 -0500

    Move

--- Striker.java
+++ Striker.java
@@ -1,67 +1,67 @@
-
 import java.awt.*;
+import java.awt.event.*;
 
-public class Striker extends Polygon implements Player{
 
-	static Image image;
-	
-	public Point[] points;
-	double rotation;
-	Point position;
+public class Striker extends Polygon implements Player {
 
-	int counter = 0;
-	
-	public Striker(Point[] shape, Point position, double rotation) {
-		super(shape, position, rotation);
-		this.points = shape;
-		this.rotation = rotation;
-		this.position = position;
-		
-		image = Toolkit.getDefaultToolkit().getImage("stick-figure-png-.png");
-	}
+  static Image image;
+  private final int SPEED = 5; // Adjust speed as needed
 
-	public void paint(Graphics brush) {
+  public Point[] points;
+  double rotation;
+  Point position;
+  private KeyListener keyListener;
 
-		brush.drawImage(image, 200, 300, 100, 100, null);	
-		/*
-		brush.setColor(Color.CYAN); // Set color to blue (for example)
+  public Striker(Point[] shape, Point position, double rotation) {
+    super(shape, position, rotation);
+    this.points = shape;
+    this.rotation = rotation;
+    this.position = position;
+    image = Toolkit.getDefaultToolkit().getImage("stick-figure-png-.png"); // Replace with your image path
 
-		Point[] points = getPoints();
+    keyListener = new KeyAdapter() {
+    @Override
+    public void keyPressed(KeyEvent e) {
+    	int keyCode = e.getKeyCode();
+        if (keyCode == KeyEvent.VK_LEFT) {
+          moveLeft();
+        } else if (keyCode == KeyEvent.VK_RIGHT) {
+          moveRight();
+        }
+      }
+    };
+  }
 
-		int[] xPoints = new int[points.length];
+  @Override
+  public void move() {
+    // Empty implementation, movement handled by key presses
+  }
 
-		int[] yPoints = new int[points.length];
+  public void paint(Graphics brush) {
+    brush.drawImage(image, (int) position.getX() - image.getWidth(null) / 2, (int) position.getY() - image.getHeight(null) / 2, null);
+  }
 
-		for (int i = 0; i < points.length; i++) {
-			xPoints[i] = (int) points[i].getX();
-			yPoints[i] = (int) points[i].getY();
-		}
-		brush.fillPolygon(xPoints, yPoints, points.length);
-		
-		brush.drawString("Counter is " + counter,10,10);
-		*/
-	}
+  public void moveLeft() {
+    if (position.getX() > 50) {
+      position.setX(position.getX() - SPEED);
+    }
+  }
 
-	public void move() {
-		double amountToMove = this.position.getX();
-		boolean forward = true;
-		boolean left = false;
-		boolean right = false;
+public void moveRight() {
+  if (position.getX() < 500) {
+    position.setX(position.getX() + SPEED);
+  }
+}
 
-		if (amountToMove < 700 && amountToMove >= 0) {
-			this.position.setX(++amountToMove);
-			//counter++;
-		}
+  public void addKeyListener(Component component) {
+    component.addKeyListener(keyListener);
+  }
 
-		if (this.position.getX() >= 700) {
-			this.position.setX(10);
-			counter++;
-		}
+  public void removeKeyListener(Component component) {
+    component.removeKeyListener(keyListener);
+  }
 
-	}
-
-	public void rotate(int degrees) {
-		rotation = (rotation + degrees) % 360;
-	}
-
+  public void rotate(int degrees) {
+    rotation = (rotation + degrees) % 360;
+  }
 }
--- YourGameName.java
+++ YourGameName.java
@@ -16,29 +16,32 @@ class YourGameName extends Game {
 	Striker striker;
 	
 	public YourGameName() {
-		super("YourGameName!", 800, 600);
+		super("Futbol!", 800, 600);
 		this.setFocusable(true);
 		this.requestFocus();
-		
+	  
 		// Create element with desired shape, position, and rotation
-		Point[] elementPoints = { 
-				new Point(0, 0), 
-				new Point(100, 0), 
-				new Point(100, 100), 
-				new Point(0, 100) };
-		
+		Point[] elementPoints = {
+			new Point(0, 0),
+			new Point(100, 0),
+			new Point(100, 100),
+			new Point(0, 100)
+		};
+	  
 		Point elementPosition = new Point(400, 300); // Center of the screen
 		double elementRotation = 0; // No rotation initially
-
+	  
 		element = new Element(elementPoints, elementPosition, elementRotation);
-		
+	  
 		striker = new Striker(elementPoints, elementPosition, elementRotation);
+		striker.addKeyListener(this); 
 	}
 
-	public void paint(Graphics brush) {
-    	brush.setColor(Color.green);
-    	brush.fillRect(0,0,width,height);
-    	
+
+  public void paint(Graphics brush) {
+    brush.setColor(Color.green);
+    brush.fillRect(0, 0, width, height);
+
     	// sample code for printing message for debugging
     	// counter is incremented and this message printed
     	// each time the canvas is repainted
@@ -46,21 +49,25 @@ class YourGameName extends Game {
     	//brush.setColor(Color.white);
     	//brush.drawString("Counter is " + counter,10,10);
     	//brush.draw
-    	element.paint(brush);
-    	element.move();
-    	element.rotate(5);
-    	brush.drawLine(600, 150 , 800, 150);
-    	brush.drawLine(600, 150 , 600, 450);
-    	brush.drawLine(600, 450 , 800, 450);
-    	brush.setColor(Color.white);
-    	striker.paint(brush);
-    	//brush.set
-    		
-    	
+
+    if (element != null) {
+      element.paint(brush);
+     // element.move(); // Call move
+      element.rotate(5); // Call rotate
+      repaint(); // Call repaint after modifications
+    }
+
+    brush.drawLine(600, 150, 800, 150);
+    brush.drawLine(600, 150, 600, 450);
+    brush.drawLine(600, 450, 800, 450);
+    brush.setColor(Color.white);
+	if (striker != null) {
+		striker.paint(brush);
+	  }
   }
 
-	public static void main(String[] args) {
-		YourGameName a = new YourGameName();
-		a.repaint();
-	}
+  public static void main(String[] args) {
+    YourGameName a = new YourGameName();
+    a.setVisible(true); 
+  }
 }
