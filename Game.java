4692b1961052f3d63a234619eb9e637a549fc911
Zyad <zhk6319@gmail.com>
2024-03-03 19:35:53 -0500

    Bugs be gone

--- Game.java
+++ Game.java
@@ -36,9 +36,11 @@ abstract class Game extends Canvas {
   // a second before repeating itself, assuming the game is on. This is done
   // to avoid a choppy painting experience if repainted in pieces.
   public void update(Graphics brush) {
-    paint(buffer.getGraphics());
-		brush.drawImage(buffer,0,0,this);
-    if (on) {sleep(10); repaint();}
+    if (this.buffer != null) {
+      paint(buffer.getGraphics());
+      brush.drawImage(buffer,0,0,this);
+      if (on) {sleep(10); repaint();}
+    }
   }
   
   // 'sleep' is a simple helper function used in 'update'.
